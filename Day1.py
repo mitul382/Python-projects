53b45addf0eee8bd94dab0e5c16ea77d102cf5b6
Md Shahriyar Al Mustakim Mitul <shahriyarmitul3737@gmail.com>
2022-10-02 01:28:39 +0600

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -2,3 +2,4 @@
 
 print('Day 1 - String Manipulation\nString Concatenation is done with the ' + '"+" sign.\n'+'e.g. print("Hello " + "world")\n'+"New lines can be created with a backslash and n.")
 print("Hey there")
+print("Hola there")
